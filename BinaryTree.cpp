6b7c4b60df40167d7f5361a9d4b4a5f6f3cc75a8
AxelPortier <axel.portier@edu.devinci.fr>
2025-10-23 09:44:29 +0200

    CRR Implémentation 1)

---
+++ BinaryTree.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "BinaryTree.h"
+
+template <typename T>
+void BinaryTree<T>::setDepth(int n) {
+    _tree.resize(n);   // Resize the number of levels
+    for (int i = 0; i < n; i++) {
+        _tree[i].resize(i + 1); //Each level has i has i+1 nodes
+    }
+    _depth = n; //Update the attribut of the class
+}
+
+template <typename T>
+void BinaryTree<T>::setNode(int n, int i, T value){ //Set the value stored in value _tree at the given indices n,i
+    if((n < _depth) && (n >= 0) && (_tree[n].size() > i) && (_tree[n].size()>=0)){
+        _tree[n][i] = value;
+    }
+    else{
+        throw std::invalid_argument("Node or Depth is incorrect");
+    }
+}
+
+template <typename T>
+T BinaryTree<T>::getNode(int n, int i) const { //Get the value of the node at the indice n,i
+    if((n < _depth) && (n >= 0) && (_tree[n].size() > i) && (_tree[n].size()>=0)){
+        return _tree[n][i];
+    }
+    else{
+        throw std::invalid_argument("Node or Depth is incorrect");
+    }
+}
+template <typename T>
+void BinaryTree<T>::display() {     //Display in a beautiful way the tree
+    for(int i=0; i < _depth; i++){
+        for(int j=0; j < _tree[i].size(); j++){
+            std::cout << _tree[i][j] <<" "; 
+        }
+        std::cout << std::endl;
+    }
+} 
---
+++ BinaryTree.h
@@ -0,0 +1,16 @@
+#ifndef BINARYTREE
+#define BINARYTREE
+
+template <typename T> //BinaryTree<int>, the compilator replace all T by int
+class BinaryTree {
+private :
+    int _depth;
+    std::vector<std::vector<T>> _tree;
+public:
+    void setDepth(int); //Set the depth of the tree and allocates/deallocate the vectors in tree 
+    void setNode(int, int, T); //Set the value stored in value _tree at the given indices n,i
+    T getNode(int, int) const ; //Get the value of the node at the indice n,i
+    void display(); //Display in a beautiful way the tree
+};
+
+#endif
