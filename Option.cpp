5447777329825c1263dd9cfd2ad321fd3e4917a6
AxelPortier <axel.portier@edu.devinci.fr>
2025-11-05 21:35:43 +0100

    Ajout main et class Option OP

--- Option.cpp
+++ Option.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include "Option.h"
 
-Option::Option(double exp) : _expiry(exp){};
-double Option::getExpiry() const{
-    return _expiry;
+Option::Option(double expiry){
+    if(expiry <= 0){
+        throw std::invalid_argument("Expiry must be positive");
+    }  
+    else{
+        _expiry = expiry;
+    }
 }
+Option::~Option(){};
 
+double Option::getExpiry() const{return _expiry;} // Return maturity
+
+//Return the payoff
 double Option::payoffPath(const std::vector<double>& path) const{
-    if (path.empty()) return 0.0;
-    return payoff(path.back());
+    if (path.empty()){
+        return 0.0;
+    }
+    else{
+        return payoff(path.back());
+    }
 }
 
-bool Option::isDigital(){
-    return false;
+std::vector<double> Option::getTimeSteps() {
+	std::vector<double> timesteps{ 0, _expiry };
+	return timesteps;
 }
-bool Option::isAsianOption(){
-    return false; 
-}
-bool Option::isAmericanOption(){
-    return false; 
-}
+
+bool Option::isDigital(){return false;}
+bool Option::isAsianOption(){return false;} 
+bool Option::isAmericanOption(){return false;}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "CallOption.h"
+#include "PutOption.h"
+#include "EuropeanDigitalCallOption.h"
+#include "EuropeanDigitalPutOption.h"
+#include "BlackScholesPricer.h"
+#include "CRRPricer.h"
+#include "BinaryTree.h"
+#include <vector>
+
+int main(){
+    
+}
