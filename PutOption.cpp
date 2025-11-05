2a427a3b2c0d8f28dc7f9f9627a934e2f630a648
AxelPortier <axel.portier@edu.devinci.fr>
2025-11-05 23:46:37 +0100

    a

--- PutOption.cpp
+++ PutOption.cpp
@@ -6,4 +6,4 @@ PutOption::~PutOption(){}
 
 double PutOption::payoff(double spot) const {return std::max(getStrike()-spot , 0.0);} // Payoff function for put option : max(K - z, 0)
 
-OptionType PutOption::GetOptionType() const {return OptionType::Put;} // Return option type as call
+OptionType PutOption::getOptionType() const {return OptionType::Put;} // Return option type as call
