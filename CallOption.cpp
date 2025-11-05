f15fd0017ce6c44e1dd726befc50773d3d31c80f
AxelPortier <axel.portier@edu.devinci.fr>
2025-11-05 22:07:29 +0100

    juste un ptit truc mieux

--- CallOption.cpp
+++ CallOption.cpp
@@ -1,17 +1,10 @@
 #include "CallOption.h"
+#include <algorithm>
 
 CallOption::CallOption(double expiry, double strike) : EuropeanVanillaOption(expiry, strike) {}
 CallOption::~CallOption() {}
 
 // Payoff function for call option : max(z - K, 0)
-double CallOption::payoff(double spot) const { 
-    double K = getStrike();
-    if (spot < K){
-        return 0.0;
-    }
-    else {
-        return spot-K;
-    }
-}
+double CallOption::payoff(double spot) const {return std::max(spot-getStrike(), 0.0);}
 
 OptionType CallOption::getOptionType() const {return OptionType::Call;} // Return option type as call
