3644e58933a13aaae96fed47ae7547d5b8ee8cf8
francis983 <francis.tinval@edu.devinci.fr>
2025-12-03 17:04:08 +0100

    Ajout des commentaires BlackScholesPricer.h

--- BlackScholesPricer.h
+++ BlackScholesPricer.h
@@ -10,13 +10,13 @@ private:
     double _volatility; // Volatility
 
 public:
-    BlackScholesPricer(Option* option, double asset_price, double interest_rate, double volatility);
-    ~BlackScholesPricer();
+    BlackScholesPricer(Option* option, double asset_price, double interest_rate, double volatility);    //Constructor
+    ~BlackScholesPricer();  //Destructor
 
-    double operator()() const;
-    double delta() const;
-    double norm_cdf(double) const;
-    double norm_pdf(double) const;
-    double calculate_d1() const;
-    double calculate_d2() const;
+    double operator()() const;      //We compute the price of the option
+    double delta() const;           //The delta
+    double norm_cdf(double) const;   //The standard normal CDF
+    double norm_pdf(double) const;   //The standard normal PDF 
+    double calculate_d1() const;     
+    double calculate_d2() const;    
 };
