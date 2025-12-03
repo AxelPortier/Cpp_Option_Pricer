d1ef32893c2b30a0b559fe4b0d5d5451793cc505
francis983 <francis.tinval@edu.devinci.fr>
2025-12-03 15:58:44 +0100

    Ajout commentaires class AsianOption.cpp

--- AsianOption.h
+++ AsianOption.h
@@ -3,14 +3,15 @@
 #include "Option.h"
 #include <vector>
 
-class AsianOption : public Option {
+class AsianOption : public Option {   //Asian Option class derived from Option
 private:
-	std::vector<double> timeSteps;
+	std::vector<double> timeSteps;    //The Monitoring time points
 public:
-    AsianOption(double expiry, const std::vector<double> monitoringTimes);
+    AsianOption(double expiry, const std::vector<double> monitoringTimes);   //The constructor 
 private:
-    const std::vector<double>& getTimeSteps() const;
-    Override AsianOption::payoffPath(std::vector<double>) const {
+    const std::vector<double>& getTimeSteps() const;        //Time-steps's access on Read-Only
+    
+    Override AsianOption::payoffPath(std::vector<double>) const {       //Computes payoff from a price path
         
     }
 
